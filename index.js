98c9e1fad39f98393f658171ad83de937828d37e
farhah din <farhahdin97@hotmail.com>
2021-12-16 19:47:11 +0000

    arrays

---
+++ index.js
@@ -0,0 +1,7 @@
+let students = ["Humma", "Girry", "Samaya" , "Nadira", "Fatima"];
+
+console.log(students)
+
+for(let i = 0; i < students.length; i++){
+    console.log("GREAT TO SEE YOU " + students[i]);
+  }
